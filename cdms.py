1ba30ffcc701d3796a2c5390d9e91c30734ce095
Prabhu N <prabhu3123@gmail.com>
2023-12-19 07:44:28 +0530

    27

--- cdms.py
+++ cdms.py
@@ -167,7 +167,7 @@ for i in file1:
     
             "fileName":"CDMS_output.csv",
     
-            "filePath":i.replace(config['replace_string'],config['replace_with']),
+            "filePath":i.replace(config['replace_string'],config['replace_with'])+'/',
     
             "subListID":85,
     
@@ -185,41 +185,45 @@ for i in file1:
         upload_id = response.json()['content']['uploadId']
 
 
-#         try:
-
-#             producer = KafkaProducer(bootstrap_servers='MR402S0352D.palawangroup.com:9092')
-
-#             topic = 'ftpKafkaConsumer'
-         
-# #            my_dict = {'fileUploadId': upload_id, 'filePath': i.replace(config['replace_string'],config['replace_with']), 'fileName': 'CDMS_value.csv'}
-#             my_dict = {'fileUploadId': 314, 'filePath': '/STFS0029M/1491702726149369/', 'fileName': 'sampleDoc (98).csv'}
-
-#             my_dict = json.dumps(my_dict)
-
-#             producer.send(topic, value=my_dict.encode('utf-8'))
-
-#             print("Message sent successfully")
-         
-#         except Exception as e:
-
-#             print(f"Error: {e}")
-        
-                
         try:
+
             producer = KafkaProducer(bootstrap_servers='MR402S0352D.palawangroup.com:9092')
+
             topic = 'ftpKafkaConsumer'
-        
-            for i in range(1):
-                message = f"Message {i}"
-                my_dict = {'fileUploadId': 314, 'filePath': '/STFS0029M/1491702726149369/', 'fileName': 'sampleDoc (98).csv'}
-                my_dict = json.dumps(my_dict)
-                producer.send(topic, value=my_dict.encode('utf-8'))
-                print("Message sent successfully")
-        
+         
+            my_dict = {'fileUploadId': upload_id, 'filePath': i.replace(config['replace_string'],config['replace_with'])+"/", 'fileName': 'CDMS_value.csv'}
+            # my_dict = {'fileUploadId': 314, 'filePath': '/STFS0029M/1491702726149369/', 'fileName': 'sampleDoc (98).csv'}
+
+            my_dict = json.dumps(my_dict)
+
+            producer.send(topic, value=my_dict.encode('utf-8'))
+
+            print("Message sent successfully")
+         
         except Exception as e:
+
             print(f"Error: {e}")
+        
         finally:
             producer.close()
+
+
+                
+        # try:
+        #     producer = KafkaProducer(bootstrap_servers='MR402S0352D.palawangroup.com:9092')
+        #     topic = 'ftpKafkaConsumer'
+        
+        #     for i in range(1):
+        #         message = f"Message {i}"
+        #         my_dict = {'fileUploadId': 314, 'filePath': '/STFS0029M/1491702726149369/', 'fileName': 'sampleDoc (98).csv'}
+        #         my_dict = json.dumps(my_dict)
+        #         producer.send(topic, value=my_dict.encode('utf-8'))
+        #         print("Message sent successfully")
+        
+        # except Exception as e:
+        #     print(f"Error: {e}")
+        # finally:
+        #     producer.close()
                 
         
         
